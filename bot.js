50531b7c10276771583aeef7aefa09c206888fa7
Miguel Corbacho <mikelcrash63@gmail.com>
2025-03-16 22:04:44 +0100

    arreglo Token

--- bot.js
+++ bot.js
@@ -1,25 +1,23 @@
 require("dotenv").config();
 const express = require("express");
-//const TelegramBot = require("node-telegram-bot-api");
-const { Telegraf } = require('telegraf');
 
-const TOKEN = process.env.TELEGRAM_TOKEN;
+//const TelegramBot = require("node-telegram-bot-api");
 //const bot = new TelegramBot(TOKEN, { polling: false });
 
-const bot = new Telegraf('TU_TOKEN_AQUI');
+const { Telegraf } = require('telegraf');
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const bot = new Telegraf(TOKEN);
 // Para que no use pooling  el bot de teleggram porque consume mas recursos
 bot.telegram.setWebhook('https://mikorh.ddns.net/webhook');
 
-// Iniciamos la base de datos
+// ------ Iniciamos la base de datos ------ // 
 const mysql = require('mysql2');
-
 const connection = mysql.createConnection({
-    host: process.env.HOST,
+    host: process.env.HOST_DB,
     user: process.env.USER_DB,
     password: process.env.PASS_DB,
     database: process.env.TLBOT_DB
 });
-
 connection.connect(err => {
     if (err) {
         console.error('Error conectando a la DB:', err);
@@ -27,11 +25,12 @@ connection.connect(err => {
     }
     console.log('Conectado a MariaDB ✅');
 });
+// ------------------------------------ //
 
 // Endpoint del bot
 const app = express();
 app.use(express.json());
-app.use(bot.webhookCallback('/webhook'));
+app.use(bot.webhookCallback('/webhook')); // Usa webhooks en vez de pooling
 
 // Telegraf
 bot.start((ctx) => ctx.reply('¡Bienvenido! 🤖'));
@@ -44,16 +43,7 @@ bot.command('info', (ctx) => {
 bot.launch();
 console.log('Bot iniciado 🚀');
 
-// Telegrgam normal
-//app.post(`/bot${TOKEN}`, (req, res) => {
-//    bot.processUpdate(req.body);
-//    res.sendStatus(200);
-//});
-
-//bot.on("message", (msg) => {
-//    bot.sendMessage(msg.chat.id, "Hola! Soy tu bot de Telegram 🤖");
-//});
-
+// Escuchamos en el puerto 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Bot escuchando en http://localhost:${PORT}`);
