441b186ca953f3a8e2b013f988968921dee7adba
mkl-corbachoh <mikelcrash63@gmail.com>
2025-03-17 22:53:56 +0000

    Arreglo env para token weather

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-  config: process.env.PORT,
-  botToken: process.env.BOT_TOKEN,
+  port: process.env.PORT,
+  botToken: process.env.TELEGRAM_TOKEN,
   weatherApiKey: process.env.WEATHER_API_KEY,
   hostDb: process.env.HOST_DB,
   userDb: process.env.USER_DB,
